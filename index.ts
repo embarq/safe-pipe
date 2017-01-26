9d9ee3a58a3a521a3377acee842337fdec605e28
embarq <embarq.dev@gmail.com>
2017-01-26 20:20:40 +0200

    Change index file extension

---
+++ index.ts
@@ -0,0 +1,10 @@
+export * from './src/safe-pipe';
+
+import { NgModule } from '@angular/core';
+import { SafePipe } from './src/safe-pipe';
+
+@NgModule({
+  declarations: [ SafePipe ],
+  exports: [ SafePipe ]
+})
+export class SafePipeModule { }
