fb86adb182d5dcb3b2ee1fd472c8f58f8c256371
embarq <embarq.dev@gmail.com>
2017-01-31 00:49:01 +0200

    Change exports

--- index.ts
+++ index.ts
@@ -1,8 +1,1 @@
-import { NgModule } from '@angular/core';
-import { SafePipe } from './src/safe-pipe';
-
-@NgModule({
-  declarations: [ SafePipe ],
-  exports: [ SafePipe ]
-})
-export class SafePipeModule { }
+export * from './src';
