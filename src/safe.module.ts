4c53413b7e309e2977df60070632df5d36260219
embarq <embarq.dev@gmail.com>
2017-02-03 19:55:31 +0200

    Import @angular/common module

--- src/safe.module.ts
+++ src/safe.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { SafePipe } from './safe.pipe';
 
 @NgModule({
   declarations: [ SafePipe ],
-  exports: [ SafePipe ]
+  exports: [ SafePipe ],
+  imports: [ CommonModule ]
 })
 export class SafePipeModule { }
