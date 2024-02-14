cb2e3595cf8a9eed3a9b227c5f74f0ed123077bd
Raphaël Balet <raphael@megaphone.info>
2024-02-14 14:19:24 +0100

    add: .prettierrc

--- projects/safe-pipe/src/lib/safe-pipe.pipe.ts
+++ projects/safe-pipe/src/lib/safe-pipe.pipe.ts
@@ -1,26 +1,32 @@
-import { Pipe, PipeTransform } from "@angular/core";
-import { DomSanitizer, SafeHtml, SafeResourceUrl, SafeScript, SafeStyle, SafeUrl } from "@angular/platform-browser";
+import { Pipe, PipeTransform } from '@angular/core';
+import { DomSanitizer, SafeHtml, SafeResourceUrl, SafeScript, SafeStyle, SafeUrl } from '@angular/platform-browser';
 
-export type SafePipeType = "html" | "style" | "script" | "url" | "resourceUrl";
+// prettier-ignore
+export type SafePipeType
+= 'html'
+| 'style'
+| 'script'
+| 'url'
+| 'resourceUrl';
 
 @Pipe({
   standalone: true,
-  name: "safe",
+  name: 'safe',
 })
 export class SafePipe implements PipeTransform {
   constructor(protected sanitizer: DomSanitizer) {}
 
   public transform(value: string, type: SafePipeType): SafeHtml | SafeStyle | SafeScript | SafeUrl | SafeResourceUrl {
     switch (type) {
-      case "html":
+      case 'html':
         return this.sanitizer.bypassSecurityTrustHtml(value);
-      case "style":
+      case 'style':
         return this.sanitizer.bypassSecurityTrustStyle(value);
-      case "script":
+      case 'script':
         return this.sanitizer.bypassSecurityTrustScript(value);
-      case "url":
+      case 'url':
         return this.sanitizer.bypassSecurityTrustUrl(value);
-      case "resourceUrl":
+      case 'resourceUrl':
         return this.sanitizer.bypassSecurityTrustResourceUrl(value);
       default:
         throw new Error(`SafePipe unable to bypass security for invalid type: ${type}`);
